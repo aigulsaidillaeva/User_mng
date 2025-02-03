9aad06c9022dd7ad3a9f4675de87638b8689eaa6
Aigul <saidillaeva saidillaevaaigul@gmail.com>
2025-02-03 17:09:47 +0600

    userForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
-import Table from "./components/table/table";
+
 import Button from "./components/UI/Button";
+import Form from "./components/form/Form";
+import { Modal } from "./components/UI/modal";
 
 const USERS = [
   { id: "c1", user: "Atambaed", email: "aa@gmail.com", password: "1234" },
   { id: "c2", user: "Atambaed", email: "aa@gmail.com", password: "1234" },
 ];
 const App = () => {
-  const [modal, setModal] = useState(true);
   return (
     <div>
       <Button variant="default" title="ADD USER" />
-      <Table users={USERS}></Table>
+
+      <Table users={USERS} />
+      <Modal open={true}>
+        <Form />
+      </Modal>
     </div>
   );
 };
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Input from "../UI/input";
+import styled from "styled-components";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = (props) => {
+  return (
+    <StyledForm>
+      <StyledBox>
+        <Input type="email" placeholdor="Write your email" />
+      </StyledBox>
+      <StyledBox>
+        <Input type="password" placeholdor="Write your password" />
+      </StyledBox>
+      <StyledBox>
+        <Input type="text" placeholdor="Write your text" />
+      </StyledBox>
+    </StyledForm>
+  );
 };
+const StyledForm = styled.form``;
+const StyledBox = styled.div`
+  margin-bottom: 15px;
+`;
 
 export default Form;
